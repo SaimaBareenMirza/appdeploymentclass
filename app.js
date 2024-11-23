3f9a1649cbbf9b99dafe1408565e8a255be5bb0e
Saima <bareensaima2512@gmail.com>
2024-11-23 10:05:23 -0500

    Initial commits

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req,res) => {
-    res.send("Hello world")
+    res.send("Hello world One")
 });
 app.get('/test', (req,res) => {
     res.send("Hello world test")
